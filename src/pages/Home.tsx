d5e9dec2ed9435b9add8586ebdd7a93477da4034
Bruno Machado <brunomdr46@gmail.com>
2021-11-30 17:24:33 -0300

    Adicionado componente de oferta

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Oferta from '../components/Oferta'
 import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
     <div>
+      <Oferta/>
       <Navbar/>
     </div>
   )
