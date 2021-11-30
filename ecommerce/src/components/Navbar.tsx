de9c76e673897b52c350ed60c81b5b7d6c81cba4
Bruno Machado <brunomdr46@gmail.com>
2021-11-30 14:49:16 -0300

    Primeiro commit

---
+++ ecommerce/src/components/Navbar.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  padding: 10px 20px;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+
+const Language = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const Center = styled.div`
+  flex: 1;
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
+
+export default function Navbar() {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>PT-BR</Language>
+          <SearchContainer>
+            
+          </SearchContainer>
+        </Left>
+        <Center>center</Center>
+        <Right>right</Right>
+      </Wrapper>
+    </Container>
+  );
+}
